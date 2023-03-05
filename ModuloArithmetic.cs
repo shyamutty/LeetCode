55b17aedbc3099b360216d452781f4826e9f1ef8
ShyamNS <shyamutty@gmail.com>
2023-03-05 22:55:47 +0530

    Update ModuloArithmetic.cs

--- ModuloArithmetic.cs
+++ ModuloArithmetic.cs
@@ -17,9 +17,24 @@ int FastPower(int a, int b)
     return res;
 }
   
+//(a + b) % n = (a%n + b%n) % n
+//(a * b) % n = (a%n * b%n) % n
 
-
-
+long FastPower(long a, long b, int n)
+{
+  long res = 1;
+  while(b > 0)
+  {
+    if((b&1) != 0)
+    {
+      //res = (res % n * a % n) % n; //res % n = res because ( 3%5 = 3)
+      res = (res * a % n) % n;
+    }
+    a = (a % n * a % n) % n;
+    b = b >> 1;
+  }
+  return res;
+}
 
 
 //https://www.youtube.com/watch?v=CnPreli2F-E&list=PLUcsbZa0qzu3yNzzAxgvSgRobdUUJvz7p&index=7
