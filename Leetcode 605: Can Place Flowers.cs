6d1da85b8db4a7e80e46a464e410a71dee0a5e27
ShyamNS <shyamutty@gmail.com>
2023-03-20 10:19:49 +0530

    Update Leetcode 605: Can Place Flowers.cs

--- Leetcode 605: Can Place Flowers.cs
+++ Leetcode 605: Can Place Flowers.cs
@@ -4,7 +4,7 @@ public class Solution {
 
         for(int i = 0; i < length; i++)
         {
-            if(flowerbed[i]==0 && (i<0 || flowerbed[i-1] == 0) && (i==length-1 || flowerbed[i+1] == 0))
+            if(flowerbed[i]==0 && (i==0 || flowerbed[i-1] == 0) && (i==length-1 || flowerbed[i+1] == 0))
             {
                 flowerbed[i] = 1;
                 n -= 1;
@@ -15,3 +15,7 @@ public class Solution {
         return n<=0;
     }
 }
+
+//idea: you need 1-0-1-0 etc sequence
+//note: n is new flowers that can be planted
+//first and last elemtents of array are edge conditions
