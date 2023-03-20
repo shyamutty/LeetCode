8f3d41d88f0724b02bd26864fa146cf40dd452b3
ShyamNS <shyamutty@gmail.com>
2023-03-20 16:24:17 +0530

    Create 198. House Robber(DP).cs

---
+++ 198. House Robber(DP).cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int Rob(int[] nums) {
+        int rob1 = 0, rob2 = 0;
+
+        for(int i = 0; i< nums.Length; i++)
+        {
+            var temp = Math.Max(nums[i] + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+
+        return rob2;
+    }
+}
