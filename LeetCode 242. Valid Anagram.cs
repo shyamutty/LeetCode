794f21daa1d1fb760ce7bd1742ba0c2902292b77
ShyamNS <shyamutty@gmail.com>
2023-03-21 12:10:18 +0530

    Update LeetCode 242. Valid Anagram.cs

--- LeetCode 242. Valid Anagram.cs
+++ LeetCode 242. Valid Anagram.cs
@@ -34,3 +34,43 @@ public class Solution
             return true;
         }
     }
+
+//Another way, bad way of doing
+public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            Dictionary<int, int> sDict = new Dictionary<int, int>();
+            Dictionary<int, int> tDict = new Dictionary<int, int>();
+
+            if(s.Length != t.Length)
+                return false;
+
+            for (int i = 0; i< s.Length; i++)
+            {
+                if(!sDict.ContainsKey(s[i]))
+                    sDict.Add(s[i],1);
+                else
+                    sDict[s[i]] = sDict[s[i]]+1;
+                
+                if(!tDict.ContainsKey(t[i]))
+                    tDict.Add(t[i],1);
+                else
+                    tDict[t[i]] += 1;
+            }
+
+            IEnumerator<int> keyEnumerator = sDict.Keys.GetEnumerator();
+
+            while (keyEnumerator.MoveNext())
+            {
+                int key = keyEnumerator.Current;
+                if(tDict.ContainsKey(key) && tDict[key] == sDict[key])
+                {}
+                else
+                    return false;
+            }
+
+            return true;
+        }
+    }
+
