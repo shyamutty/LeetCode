a706ffef591747b39fcbbf6eb90a495eab8518e2
ShyamNS <shyamutty@gmail.com>
2023-03-27 22:19:34 +0530

    Update 128. Longest Consecutive Sequence.cs

--- 128. Longest Consecutive Sequence.cs
+++ 128. Longest Consecutive Sequence.cs
@@ -1,5 +1,8 @@
 //https://leetcode.com/problems/longest-consecutive-sequence/description/
 /*
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+You must write an algorithm that runs in O(n) time.
+
 Example 1:
 
 Input: nums = [100,4,200,1,3,2]
