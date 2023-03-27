56a89220e04bd54e6a09ec8799dc998fb33e18d8
ShyamNS <shyamutty@gmail.com>
2023-03-27 22:16:28 +0530

    Update 128. Longest Consecutive Sequence.cs

--- 128. Longest Consecutive Sequence.cs
+++ 128. Longest Consecutive Sequence.cs
@@ -24,3 +24,35 @@ public class Solution {
 
 
 //https://www.youtube.com/watch?v=P6RZZMu_maU
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public int LongestConsecutive(int[] nums) {
+    HashSet<int> set = new HashSet<int>(nums); // add all elements of nums to a hash set for efficient lookup
+    
+    int longestStreak = 0; // initialize longest streak to 0
+    
+    foreach (int num in set) { // iterate over each element in the set
+        if (!set.Contains(num - 1)) { // if the previous element is not in the set
+            int currentNum = num; // initialize current number to current element
+            int currentStreak = 1; // initialize current streak to 1
+            
+            while (set.Contains(currentNum + 1)) { // while the next element is in the set
+                currentNum++; // increment current number
+                currentStreak++; // increment current streak
+            }
+            
+            longestStreak = Math.Max(longestStreak, currentStreak); // update longest streak if current streak is greater
+        }
+    }
+    
+    return longestStreak; // return longest streak
+}
+
+
+/*
+This solution uses a hash set to store the elements of the input array for efficient lookup. 
+It then iterates over each element in the set and checks whether the previous element is in the set. 
+If not, it starts a streak by initializing a current number and streak to the current element and 1, respectively. 
+It then continues to increment the current number and streak as long as the next element is in the set. 
+The longest streak is updated with the maximum of the current streak and the previous longest streak. 
+The final result is the longest streak.
+*/
