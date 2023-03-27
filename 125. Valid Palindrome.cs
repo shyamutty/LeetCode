acbf6ba82b95630da576d2f2f62b44f648d4cc83
ShyamNS <shyamutty@gmail.com>
2023-03-27 21:59:17 +0530

    Update 125. Valid Palindrome.cs

--- 125. Valid Palindrome.cs
+++ 125. Valid Palindrome.cs
@@ -26,3 +26,30 @@ public class Solution {
 
 //https://www.youtube.com/watch?v=jJXJ16kPFWg
 
+
+public bool IsPalindrome(string s) {
+    s = s.ToLower(); // convert string to lowercase
+    
+    int left = 0; // initialize left pointer to index 0
+    int right = s.Length - 1; // initialize right pointer to last index of string
+    
+    while (left < right) { // while left pointer is less than right pointer
+        while (left < right && !char.IsLetterOrDigit(s[left])) { // skip non-alphanumeric characters from left pointer
+            left++;
+        }
+        
+        while (left < right && !char.IsLetterOrDigit(s[right])) { // skip non-alphanumeric characters from right pointer
+            right--;
+        }
+        
+        if (s[left] != s[right]) { // if characters at left and right pointers don't match
+            return false; // not a palindrome
+        }
+        
+        left++; // move left pointer one index to the right
+        right--; // move right pointer one index to the left
+    }
+    
+    return true; // if all characters match, it's a palindrome
+}
+
